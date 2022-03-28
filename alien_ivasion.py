384d23117e329cba1411429792fefa55562e33af
Дмитрий Орлов <orlof@mail.ru>
2022-03-28 21:56:50 +0500

    Создание класса Settings

--- alien_ivasion.py
+++ alien_ivasion.py
@@ -1,21 +1,21 @@
 import sys
 import pygame
+from settings import Settings
 
 
 class AlienInvasion:
-    '''Класс для управления ресурсами и поведением игры'''
+    """Класс для управления ресурсами и поведением игры"""
 
     def __init__(self):
-        '''Инициализирует игру и создает игровые ресурсы'''
+        """Инициализирует игру и создает игровые ресурсы"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_heigth))
         pygame.display.set_caption('Alien Invasion')
-        # Назначение цвета фона
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
-        '''Запуск основного цикла игры'''
+        """Запуск основного цикла игры"""
 
         while True:
             # Отслеживание событий клавиатуры и мыши
@@ -24,10 +24,11 @@ class AlienInvasion:
                     sys.exit()
 
             # При каждом проходе цикла прорисовывается экран
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             # Отображение последнего прорисованного экрана
             pygame.display.flip()
+
 
 if __name__ == '__main__':
     # Создание экземпляра и запуск игры
