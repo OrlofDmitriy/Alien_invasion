609f5f520bf7137e0ca031f99829a4c546eebf08
Дмитрий Орлов <orlof@mail.ru>
2022-03-28 22:23:38 +0500

    Вывод корабля на экран

--- alien_ivasion.py
+++ alien_ivasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -14,6 +15,8 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_heigth))
         pygame.display.set_caption('Alien Invasion')
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Запуск основного цикла игры"""
 
@@ -25,6 +28,7 @@ class AlienInvasion:
 
             # При каждом проходе цикла прорисовывается экран
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Отображение последнего прорисованного экрана
             pygame.display.flip()
