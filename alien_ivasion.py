b2dae2395105138dbcac68b398e79bca5dda4d0e
Дмитрий Орлов <orlof@mail.ru>
2022-03-28 22:33:54 +0500

    Рефакторинг: выделение из метода run_game() вспомогательных методов _check_events() и _update_screen()

--- alien_ivasion.py
+++ alien_ivasion.py
@@ -19,19 +19,22 @@ class AlienInvasion:
 
     def run_game(self):
         """Запуск основного цикла игры"""
-
         while True:
-            # Отслеживание событий клавиатуры и мыши
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # При каждом проходе цикла прорисовывается экран
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _check_events(self):
+        """Отслеживание событий клавиатуры и мыши"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            # Отображение последнего прорисованного экрана
-            pygame.display.flip()
+    def _update_screen(self):
+        """Обновляет изображение на экране и отображает новый экран"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
